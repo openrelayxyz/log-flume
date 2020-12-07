c2178a9d0e336720983cd0a82c5b8eecdb79e49c
Austin Roberts <code@ausiv.com>
2020-12-07 10:02:37 -0600

    Ordered processor fixes
    Fix race conditions around the conclusion of the ordered processor,
    handle signals

--- flumeserver/datafeed/util.go
+++ flumeserver/datafeed/util.go
@@ -1,26 +1,49 @@
 package datafeed
 
+import (
+  "os"
+  "os/signal"
+  "syscall"
+  "log"
+)
+
 func OrderedProcessor(start uint64, buffer int, itemGetter func(uint64, chan<- interface{}, func()), orderedProcessor func(interface{})) {
   orderedResults := make(chan chan interface{}, buffer)
   semaphore := make(chan struct{}, buffer)
   quit := make(chan struct{})
+  sigs := make(chan os.Signal, 1)
+  signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
   go func() {
     for i := start ; ; i++ {
       // Add quit chan
       select {
+      case <-sigs:
+        log.Printf("Received Interrupt, stopping...")
+        semaphore <- struct{}{}
+        return
       case <-quit:
         close(orderedResults)
-        sempahore <- struct{}{}
+        semaphore <- struct{}{}
         return
       case semaphore <- struct{}{}:
       }
       ch := make(chan interface{})
       orderedResults <- ch
-      go itemGetter(i, ch, func() { quit <- struct{}{} })
+      quitter := func() {
+        select {
+        case quit <- struct{}{}:
+        default:
+        }
+        ch <- nil
+      }
+      go itemGetter(i, ch, quitter)
     }
   }()
   for ch := range orderedResults {
     <-semaphore
-    orderedProcessor(<-ch)
+    item := <-ch
+    if item != nil {
+      orderedProcessor(item)
+    }
   }
 }
